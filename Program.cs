ae1d1dd6cbff1468433bd06637c878650a3aef74
Jason Figueroa <jason.figueroa2@gmail.com>
2018-07-27 21:27:52 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FacadeTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Facade Tutorial
+
+Tutorial can be found at [Facade Tutorial][facade-tutorial]
+
+[facade-tutorial]: https://visualstudiomagazine.com/articles/2013/06/18/the-facade-pattern-in-net.aspx
+
